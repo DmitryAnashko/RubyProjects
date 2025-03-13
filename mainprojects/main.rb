000a41b2b896770ed7b2ac66e9bbbd6b7abf0b62
Dmitry Anashko <dmitryanashko@gmail.com>
2025-03-13 11:18:30 +0200

    first test

---
+++ mainprojects/main.rb
@@ -0,0 +1,13 @@
+# name = "Jack"
+# if  name == "John"
+#   puts "Hello, #{name}"
+  
+# elsif name == "Jack"
+#   puts "Hello, #{name}"
+  
+# else
+#   puts "Hello, stranger"          
+  
+# end
+puts "Hello world"
+p "Hello world"
